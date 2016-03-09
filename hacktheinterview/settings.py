9447e776d0fa339c5f2a7bfc0f8ad140ee57e1cd
Radhakrishnan Venkataramani <rad@zenefits.com>
2016-03-09 02:13:36 -0800

    Revert "lets try fixing circleci build"
    This reverts commit 98fd97c785126a2549596705f6d07de38fa9fe4c.

--- hacktheinterview/settings.py
+++ hacktheinterview/settings.py
@@ -77,8 +77,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ht',
-	'USER': 'ubuntu',
-	'PASSWORD': '',
+	'USER': 'hack_user',
+	'PASSWORD': 'hack_pass',
 	'HOST': '',
 	'PORT': '',
     }
