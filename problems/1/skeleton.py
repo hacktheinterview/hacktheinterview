a0baa148aed19dd875b934f7c915e1ba3790a084
Rad Kris <radhakrishnancegit@gmail.com>
2016-04-02 15:32:25 -0700

    python

---
+++ problems/1/skeleton.py
@@ -0,0 +1,9 @@
+# ListNode type
+# class ListNode:
+#     def __init__(self, val):
+#         self.val = val
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head):
+        # Return the reversed Head here
