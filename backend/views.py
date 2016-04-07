8c53ba3bd1c41c60cfd89018f48c4e83dc140e8d
Rad Kris <radhakrishnancegit@gmail.com>
2016-04-06 22:26:09 -0700

    boom

--- backend/views.py
+++ backend/views.py
@@ -259,7 +259,7 @@ def postSubmissionToEngine(submission):
 		memory_limit=limits['memory_limit'],
 		async=1,
 		id=submission.id,
-		callback='https://fsuvlizmfc.localtunnel.me/test_url/',
+		callback='https://sheltered-ocean-78784.herokuapp.com/test_url/',
 		compressed=0,
 	)
 
