950437f2cad8a983923d7847dc87dbe22e8331a1
Rad Kris <radhakrishnancegit@gmail.com>
2016-06-06 14:04:58 -0600

    problem no 44 lca of binary tree

---
+++ problems/44/footer.cpp
@@ -0,0 +1,82 @@
+string toStr(int x) {
+    stringstream s;
+    s<<x;
+    return s.str();
+}
+
+void preorder(TreeNode* root, stringstream& ss) {
+    if (!root) {
+        ss << ("null") << " ";
+        return;
+    }
+    ss << (toStr(root->val)) << " ";
+    preorder(root->left, ss);
+    preorder(root->right, ss);
+}
+
+string serializeTree(TreeNode* root) {
+    stringstream ss;
+    preorder(root, ss);
+    string ret = ss.str();
+    return ret;
+}
+
+int toInt(string x) {
+    stringstream s(x);
+    int y;
+    s>>y;
+    return y;
+}
+
+TreeNode* deserialize(const vector<string>& tokens, int& index) {
+    if (index >= (int)tokens.size())
+        return NULL;
+
+    string val = tokens[index];
+    index++;
+    if (val == "#") {
+        return NULL;
+    }
+
+    TreeNode * root = new TreeNode(toInt(val));
+    root->left = deserialize(tokens, index);
+    root->right = deserialize(tokens, index);
+    return root;
+}
+
+TreeNode* deserializeTree(const vector<string>& tokens) {
+    int index = 0;
+    return deserialize(tokens, index);
+}
+
+void print2DVector(const vector<vector<int>>& result) {
+    cout << "[";
+    for (const auto& row: result) {
+        cout << "[";
+        for (const auto& element: row) {
+            cout << element << ",";
+        }
+        cout << "],";
+    }
+    cout << "]" << endl;
+}
+
+int main() {
+	int testcases;
+	cin >> testcases;
+
+	for (int t = 0; t < testcases; t++) {
+        int n; cin >> n;
+        vector<string> inputItems(n);
+        for (int i = 0; i < n; i++) {
+            cin >> inputItems[i];
+        }
+        int A, B;
+        cin >> A >> B;
+        TreeNode *root = deserializeTree(inputItems);
+        Solution s;
+        auto result = s.lca(root, A, B);
+        cout << result << endl;
+	}
+	return 0;
+}
