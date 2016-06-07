eae57269b20f4ce89d0201151b21f4bd1af22d98
Rad Kris <radhakrishnancegit@gmail.com>
2016-06-07 09:58:39 -0700

    problem-54

---
+++ problems/54/footer.c
@@ -0,0 +1,13 @@
+int main() {
+	setbuf(stdout, NULL);
+	int testcases;
+	scanf("%d", &testcases);
+	int t = 0;
+	for (t = 0; t < testcases; t++) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+		int result = rangeBitwiseAnd(a, b);
+		printf("%d\n", result);
+	}
+	return 0;
+}
---
+++ problems/54/skeleton.py
@@ -0,0 +1,3 @@
+class Solution:
+    def divide(self, m, n):
+        # Implement the solution
